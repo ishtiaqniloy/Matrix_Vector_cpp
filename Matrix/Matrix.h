7da73bf77d54657fec466c4cd28d61038aaa85c1
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-07-11 00:59:11 +0600

    overloading operators

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -37,12 +37,131 @@ public:
             printf("SIZE MISMATCH!!!");
             return;
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; ++j) {
                 arr[i][j] = new_arr[i][j];
             }
         }
+    }
+
+    void setVal(int i, int j, T val){
+        if(i >= n || j >= m){
+            return;
+        }
+        arr[i][j] = val;
+    }
+
+    Matrix operator + (Matrix const &obj) {
+        if(n != obj.n || m != obj.m){
+            printf("SIZE MISMATCH!!!");
+            exit(1);
+        }
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] + obj.arr[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator - (Matrix const &obj) {
+        if(n != obj.n || m != obj.m){
+            printf("SIZE MISMATCH!!!");
+            exit(1);
+        }
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] - obj.arr[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    Matrix operator + (T const &val) {
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] + val;
+            }
+        }
+        return result;
+    }
+
+    Matrix operator - (T const &val) {
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] - val;
+            }
+        }
+        return result;
+    }
+
+    void operator = (Matrix const &obj) {
+        n = obj.n;
+        m = obj.m;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                arr[i][j] = obj.arr[i][j];
+            }
+        }
+    }
+
+    Matrix operator * (Matrix const &obj) {     //matrix multiplication
+        if(m != obj.n){
+            printf("SIZE MISMATCH!!!");
+            exit(1);
+        }
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] + obj.arr[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    Matrix operator * (T const &val) {     //scalar multiplication
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] * val;
+            }
+        }
+
+        return result;
+    }
+
+    Matrix operator / (T const &val) {     //scalar division
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] / val;
+            }
+        }
+
+        return result;
+    }
+
+
+
+    Matrix elementWiseProduct (Matrix const &obj) {
+        if(n != obj.n || m != obj.m){
+            printf("SIZE MISMATCH!!!");
+            exit(1);
+        }
+        Matrix<T> result(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                result.arr[i][j] = arr[i][j] * obj.arr[i][j];
+            }
+        }
+
+        return result;
     }
 
 
@@ -56,6 +175,30 @@ public:
             printf("\n");
         }
 
+    }
+
+    T **getArr() const {
+        return arr;
+    }
+
+    void setArr(T **arr) {
+        Matrix::arr = arr;
+    }
+
+    int getN() const {
+        return n;
+    }
+
+    void setN(int n) {
+        Matrix::n = n;
+    }
+
+    int getM() const {
+        return m;
+    }
+
+    void setM(int m) {
+        Matrix::m = m;
     }
 
 };
--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -14,7 +14,24 @@ int main() {
 
     Matrix<double> A(3, 3);
 
-    A.printMatrix();
+    Matrix<double> B(3, 4);
+
+    A.setVal(0, 0, 5);
+    B.setVal( 1, 2, 9);
+
+    Matrix<double > C = A+B-7.5;
+
+    int tempArr[3][3];
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            tempArr[i][j] = i+j;
+        }
+    }
+
+//    C.setValues(3, 3, (int **)tempArr);
+
+    C.printMatrix();
+
 
 
     return 0;
