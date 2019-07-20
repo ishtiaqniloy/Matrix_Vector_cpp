1f0b0a6ba542a58ec5d5064a5a5022f76f617272
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-07-20 12:37:33 +0600

    point getter setter added

--- Matrix_Vector/Point3D.h
+++ Matrix_Vector/Point3D.h
@@ -5,8 +5,8 @@
 #include "Vector3D.h"
 
 class Point3D{
-public:
     double x, y, z;
+public:
 
     Point3D(double varX = 0, double varY  = 0, double varZ = 0){
         x = varX;
@@ -14,11 +14,35 @@ public:
         z = varZ;
     }
 
+    double getX() const {
+        return x;
+    }
+
+    void setX(double x) {
+        Point3D::x = x;
+    }
+
+    double getY() const {
+        return y;
+    }
+
+    void setY(double y) {
+        Point3D::y = y;
+    }
+
+    double getZ() const {
+        return z;
+    }
+
+    void setZ(double z) {
+        Point3D::z = z;
+    }
+
     Vector3D operator - (Point3D const &obj) {
         Vector3D result;
-        result.setX(x - obj.x);
-        result.setY(y - obj.y);
-        result.setZ(z - obj.z);
+        result.setX(x - obj.getX());
+        result.setY(y - obj.getY());
+        result.setZ(z - obj.getZ());
 
         return result;
     }
